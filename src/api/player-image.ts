14169e8de2951fdc04b6656ad0e1bf296e9be141
Lubwama Collins <bizzicole87@example.com>
2025-02-04 18:11:51 +0300

    Fix: Dead weight burned

--- src/api/player-image.ts
+++ src/api/player-image.ts
@@ -1,6 +1,3 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
-
 export async function getPlayerImage(playerId: number) {
   try {
     const imageUrl = `https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/${playerId}/headshot/67/current`;
